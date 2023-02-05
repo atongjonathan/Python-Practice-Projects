89ab9ba3d0c8d9dac8d282f2e6db68a3bb559551
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-02-05 15:23:50 +0300

    modified practice

--- practice.py
+++ practice.py
@@ -1,9 +1,9 @@
-def paperwork(n, m):
-    if m > 0 and n > 0 :
-        result = n*m
-    else:
-        result = 0
-    return result
+import math
+def is_triangle(a, b, c):
+    s = a+b+c
+    Area = math.sqrt(s*((s-a)+(s-b)+(s-c)))
+
+    return False if Area > 0 else True
 
 
-print(paperwork(-5,5))
+print(is_triangle(2,2,4))
