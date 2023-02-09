791ea6b07e028b2091a817bc2c18f10bdbe09e48
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-02-09 22:55:35 +0300

    Modified hangman

--- practice.py
+++ practice.py
@@ -1,9 +1,36 @@
-import math
-def is_triangle(a, b, c):
-    s = a+b+c
-    Area = math.sqrt(s*((s-a)+(s-b)+(s-c)))
+from words import words
+import random
+import string
+def get_valid_words(words):
+    word = random.choice(words)
+    if ' ' in words or '-' in words:
+        word = random.choice(words)
+    return word
 
-    return False if Area > 0 else True
+def hangman():
+    word = get_valid_words(words)
+    used_letters = set()
+    alphabet = set(string.ascii_uppercase)
+    word_letters = set(word.upper())
+    correctly_guessed = set()
 
 
-print(is_triangle(2,2,4))
+    #getting user input
+    
+    user_letter = input("Guess a letter: \n").upper()
+    used_letters.add(user_letter)
+    word_list = [letter if letter in word ] 
+    if user_letter in alphabet:
+        
+        print(f"You have used these letters: "' '.join(used_letters))
+        if user_letter in word:
+            print("Nice try")
+            correctly_guessed.add(user_letter)
+            word_letters.remove(user_letter)
+        else:
+            print("Not quite try again")
+    else:
+        print(f"Invalid character{user_letter}")
+
+
+hangman()
