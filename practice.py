a50699f1ed109a4db73e2c6b747f24b65d03b98d
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-02-11 22:46:17 +0300

    modified practice

--- practice.py
+++ practice.py
@@ -1,36 +1,10 @@
-from words import words
-import random
 import string
-def get_valid_words(words):
-    word = random.choice(words)
-    if ' ' in words or '-' in words:
-        word = random.choice(words)
-    return word
+word = "abcd"
+word_arr = []
+i=1
+for letter in word:
+    word2 = letter*i
+    word_arr.append(word2.capitalize())
+    i+=1
 
-def hangman():
-    word = get_valid_words(words)
-    used_letters = set()
-    alphabet = set(string.ascii_uppercase)
-    word_letters = set(word.upper())
-    correctly_guessed = set()
-
-
-    #getting user input
-    
-    user_letter = input("Guess a letter: \n").upper()
-    used_letters.add(user_letter)
-    word_list = [letter if letter in word ] 
-    if user_letter in alphabet:
-        
-        print(f"You have used these letters: "' '.join(used_letters))
-        if user_letter in word:
-            print("Nice try")
-            correctly_guessed.add(user_letter)
-            word_letters.remove(user_letter)
-        else:
-            print("Not quite try again")
-    else:
-        print(f"Invalid character{user_letter}")
-
-
-hangman()
+print('-'.join(word_arr))
