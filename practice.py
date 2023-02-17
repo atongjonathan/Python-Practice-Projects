d1aa8200662dd5c43f6811a73f83c27032c9dc21
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-02-17 16:39:21 +0300

    modified some projects

--- practice.py
+++ practice.py
@@ -1,10 +1,17 @@
-import string
-word = "abcd"
-word_arr = []
-i=1
-for letter in word:
-    word2 = letter*i
-    word_arr.append(word2.capitalize())
-    i+=1
 
-print('-'.join(word_arr))
+def accum(player1, player2):
+    # p>r r>s s>p
+    if player1 == player2 :
+        return "Draw"
+    if is_win(player1,player2):
+        return "Player 1 won "
+    if is_win(player2,player1):
+        return "Player 2 won " 
+
+def is_win(player,opponent) :
+    # p>r s>p r>s
+    if (player == 'paper' and opponent == 'rock') or (player == 'scissors' and opponent == 'paper') or (player == 'rock' and opponent == 'scissors') :
+        return True
+print(accum('rock','rock'))
+
+
--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,24 +1,26 @@
 import random
 import time
-def play() :
+def play(user,computer) :
     # getting user input
     
-    user = input("Make your choice , 'r' is for Rock 's' is for Scissors and 'p' is for Paper\n")
-    computer = random.choice(['r', 'p', 's'])
-    print(f"The computer's choice was {computer}")
+    # user = input("Make your choice , 'r' is for Rock 's' is for Scissors and 'p' is for Paper\n")
+    # computer = random.choice(['rock', 'paper', 'scissors'])
     if user == computer:
+        print(f"What a coincidemce the computer's choice was also{computer}")
         return 'It\'s a tie'
     if is_win(user, computer):
+        print(f"Fortunately the  computer's choice was {computer}")
         return "You Won!"
     if is_win (computer, user):
-       return "You Lost!"
+        print(f"Unfortunately the computer's choice was {computer}")
+        return "You Lost!"
 
 
 def is_win(player,opponent) :
     # p>r s>p r>s
-    if (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p') or (player == 'r' and opponent == 's') :
+    if (player == 'paper' and opponent == 'rock') or (player == 'scissors' and opponent == 'paper') or (player == 'rock' and opponent == 'scissors') :
         return True
-print(play())
+print(play('rock','paper'))
 
   
 
